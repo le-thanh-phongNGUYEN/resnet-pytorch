ef6aa0a3c942cc154163221fd0ca826808a23247
le-thanh-phong.nguyen <phongnlt@d-soft.com.vn>
2020-09-08 10:16:55 +0700

    first commit

---
+++ train.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+import datetime
+
+import resnet
+
+parser = argparse.ArgumentParser(description='PyTorch Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+train_loader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+
+def training_loop(n_epochs, optimizer, model, loss_fn, train_loader):
+    for epoch in range(1, n_epochs + 1):
+        loss_train = 0.0
+        for imgs, labels in train_loader:
+            imgs = imgs.to(device=device)
+            labels = labels.to(device=device)
+            outputs = model(imgs)
+            loss = loss_fn(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_train += loss.item()
+        if epoch == 1 or epoch % 10 == 0:
+            print('{} Epoch {}, Training loss {}'.format(
+                datetime.datetime.now(), epoch,
+                loss_train / len(train_loader)))
+
+
+net = ResNet50()
+net = net.to(device)
+
+# model = NetResDeep(n_chans1=32, n_blocks=100).to(device=device)
+optimizer = optim.SGD(net.parameters(), lr=3e-3)
+loss_fn = nn.CrossEntropyLoss()
+
+training_loop(
+    n_epochs=100,
+    optimizer=optimizer,
+    model=net,
+    loss_fn=loss_fn,
+    train_loader=train_loader,
+)
